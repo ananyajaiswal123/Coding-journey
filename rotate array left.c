bc432c1a2a7209aced3c45eee1dc9db3c906d56e
ananyajaiswal123 <ananyajaiswal452@gmail.com>
2026-06-15 21:22:12 +0530

    Add files via upload

---
+++ rotate array left.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, temp, i;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    temp = a[0];
+
+    for(i = 0; i < n-1; i++) {
+        a[i] = a[i+1];
+    }
+
+    a[n-1] = temp;
+
+    printf("After rotation: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
