04a6298712bb789618519f7991b5a2682a4a1b18
ananyajaiswal123 <ananyajaiswal452@gmail.com>
2026-06-20 06:46:51 +0530

    Add files via upload

---
+++ check symmetric matrix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], i,j,n,flag=1;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(a[i][j] != a[j][i]){
+                flag=0;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        printf("Symmetric");
+    else
+        printf("Not Symmetric");
+
+    return 0;
+}
